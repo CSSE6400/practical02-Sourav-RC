65a664b6a52d64fcf79dca83dcc7b0b8e167824a
Sourav-RC <110149206+Sourav-RC@users.noreply.github.com>
2023-05-16 05:14:05 +0530

    new

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    from .models import db
+    from .models.todo import Todo
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
 
     from .views.routes import api
     app.register_blueprint(api)
---
+++ todo/models/todo.py
@@ -0,0 +1,30 @@
+import datatime
+from . import db
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120), nullable=True)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    deadline_at = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datatime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datatime.datetime.utcnow, onupdate=datatime.datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'completed': self.completed,
+            'deadline_at': self.deadline_at.isoformat() if self.deadline_at else None,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
+        }
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.title}>'
